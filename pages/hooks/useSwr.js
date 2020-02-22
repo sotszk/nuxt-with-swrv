bbf2debefc83cd7914ae341f01fc61efeef3f0f2
SotaSuzuki <3.3sota.suzuki@gmail.com>
2020-02-22 13:27:57 +0900

    create cacheStorage

--- pages/hooks/useSwr.js
+++ pages/hooks/useSwr.js
@@ -7,14 +7,35 @@ import {
   onServerPrefetch,
   getCurrentInstance,
   reactive,
+  onUpdated,
 } from '@vue/composition-api'
 
+import DataCache from '@/lib/cache'
+import LocalStorageCache from '@/lib/local-storage-cache'
+
+const isDocumentVisible = () => {
+  if (
+    typeof document !== 'undefined' &&
+    typeof document.visibilityState !== 'undefined'
+  ) {
+    return document.visibilityState !== 'hidden'
+  }
+  return true
+}
+
+const dataCache = new DataCache()
+const promisesCache = new DataCache()
+
 const defaultConfig = {
-  cache: null,
+  cache: dataCache,
+  revalidateOnFocus: true,
+  revalidateDebounce: 0,
+  dedupingInterval: 2000,
+  ttl: 60 * 60 * 1000,
   onError: (_, __) => {},
 }
 
-export const useSwr = (key, fetcher, config = {}) => {
+export const useSwr = (key, fn, config = {}) => {
   let unmounted = false
   const vm = getCurrentInstance()
   const isServer = vm.$isServer
@@ -24,6 +45,7 @@ export const useSwr = (key, fetcher, config = {}) => {
   }
 
   const keyRef = typeof key === 'function' ? key : ref(key)
+
   const stateRef = reactive({
     data: undefined,
     error: null,
@@ -31,27 +53,91 @@ export const useSwr = (key, fetcher, config = {}) => {
   })
 
   const revalidate = async () => {
-    const trigger = () => {
-      console.log('triggered')
+    const keyVal = keyRef.value
+    if (!isDocumentVisible) return
+    const cacheItem = config.cache.get(keyVal, config.ttl)
+    let newData = cacheItem && cacheItem.data
+
+    stateRef.isValidating = true
+    if (newData) {
+      stateRef.data = newData
+      stateRef.error = newData.error
     }
-    await setTimeout(async () => {
-      if (!unmounted) {
-        await trigger()
+
+    const trigger = async () => {
+      console.log('triggered')
+      const promiseFromCache = promisesCache.get(
+        keyVal,
+        config.dedupingInterval
+      )
+      if (!promiseFromCache) {
+        const newPromise = fn(keyVal)
+        promisesCache.set(keyVal, newPromise)
+        try {
+          newData = await newPromise
+          stateRef.data = newData
+        } catch (err) {
+          stateRef.error = err
+          config.onError(err, keyVal)
+        }
+        stateRef.isValidating = false
+      } else {
+        newData = await promiseFromCache.data
+        if (typeof newData.data !== 'undefined') {
+          stateRef.data = newData.data
+        }
+        if (newData.error) {
+          stateRef.error = newData.error
+          config.onError(newData.error, keyVal)
+        }
+        stateRef.isValidating = false
       }
-    }, 2000)
-    // console.log('after triggered')
+    }
+
+    if (newData && config.revalidateDebounce) {
+      setTimeout(async () => {
+        if (!unmounted) {
+          await trigger()
+        }
+      }, config.revalidateDebounce)
+    } else {
+      await trigger()
+    }
   }
 
-  let timer = null
+  const timer = null
 
   onMounted(() => {
-    timer = null
-
     const tick = async () => {}
+    if (config.revalidateOnFocus) {
+      document.addEventListener('visibilitychange', revalidate, false)
+      window.addEventListener('focus', revalidate, false)
+    }
   })
+
+  try {
+    watch(keyRef, (val) => {
+      keyRef.value = val
+      console.log('watch triggered')
+      revalidate()
+      if (timer) {
+        clearTimeout(timer)
+      }
+    })
+  } catch {
+    // do nothing
+  }
+
+  // onUpdated(() => {
+  //   console.log('keyRef:', keyRef.value)
+  // })
 
   onUnmounted(() => {
     unmounted = true
+    if (config.revalidateOnFocus) {
+      document.removeEventListener('visibilitychange', revalidate, false)
+      window.removeEventListener('focus', revalidate, false)
+    }
   })
 
   if (isServer) {
--- pages/swrv.vue
+++ pages/swrv.vue
@@ -1,16 +1,33 @@
 <template>
-  <div>
-    <div>{{ title }}</div>
-    <div><button @click="() => (count += 1)">+</button>{{ count }}</div>
-    <nuxt-link to="/">go home</nuxt-link>
+  <div class="max-w-sm max-auto p-6">
+    <div class="text-xl font-bold mb-4">{{ title }}</div>
     <div>
-      <button @click="() => revalidate()">revalidate</button>
+      <button class="btn btn-blue" @click="() => (count += 1)">+</button
+      ><span class="ml-4">{{ count }}</span>
+    </div>
+    <nuxt-link
+      class="inline-block text-blue-400 hover:text-blue-600 my-4"
+      to="/"
+      >go home</nuxt-link
+    >
+    <div>
+      <button
+        class="bg-green-400 text-white py-6 px-8 text-lg"
+        @click="() => revalidate()"
+      >
+        revalidate
+      </button>
+    </div>
+
+    <div class="mt-4">
+      <p v-if="loading" class="text-md text-teal-700">Loading...</p>
+      <p v-if="responseData">{{ JSON.stringify(responseData) }}</p>
     </div>
   </div>
 </template>
 
 <script>
-import { ref, onMounted, onUnmounted } from '@vue/composition-api'
+import { ref, onMounted, onUnmounted, watch } from '@vue/composition-api'
 import { useSwr } from './hooks/useSwr'
 
 const useHello = () => {
@@ -26,21 +43,71 @@ const useHello = () => {
   return { hello }
 }
 
+const useSample = () => {
+  const { hello } = useHello()
+  const count = ref(0)
+
+  watch((onCleanup) => {
+    document.title = `${hello.value} ${count.value}`
+    onCleanup(() => {
+      console.log('fired before document title changed')
+    })
+  })
+
+  watch(
+    () => count.value,
+    () => {
+      console.log(hello.value)
+    },
+    {
+      lazy: true,
+    }
+  )
+  return {
+    hello,
+    count,
+  }
+}
+
 export default {
   name: 'Swrv',
 
   setup() {
-    const swr = useSwr()
-    const { hello } = useHello()
-    const count = ref(0)
+    const {
+      data: responseData,
+      error: responseError,
+      isValidating: loading,
+      revalidate,
+    } = useSwr('https://qiita.com/api/v2/schema', (url) =>
+      fetch(url).then((res) => res.json())
+    )
+    // const { hello, count } = useSample()
+
+    const data = {
+      title: ref('Hello world'),
+      count: ref(0),
+      responseData,
+      responseError,
+      loading,
+    }
+
+    const methods = {
+      revalidate,
+    }
 
     return {
-      title: hello,
-      count,
-      ...swr,
+      ...data,
+      ...methods,
     }
   },
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.btn {
+  @apply font-bold py-2 px-4;
+}
+.btn-blue {
+  @apply bg-blue-400 text-white;
+}
+</style>
